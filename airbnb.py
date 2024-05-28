b2d7be0d76faa327afc2b9d134b59ac4ae2378a4
SHIBU.C <153928785+Shibu266277@users.noreply.github.com>
2024-05-28 22:37:38 +0530

    Update airbnb.py

--- airbnb.py
+++ airbnb.py
@@ -6,6 +6,10 @@ import plotly.express as px
 from streamlit_option_menu import option_menu
 from PIL import Image
 
+
+
+
+
 # Stream lit page making 
 # Displaying image in the first column
 with st.sidebar:
